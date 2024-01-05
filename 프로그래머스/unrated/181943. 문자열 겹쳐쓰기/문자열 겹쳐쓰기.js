863dcc458dd8d5661be62b2d0ad6c98dad1ecfec
tjdwns335 <151831149+tjdwns335@users.noreply.github.com>
2024-01-05 19:02:10 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,10 @@
+function solution(my_string, overwrite_string, s) {
+    var answer = '';
+    var answer = '';
+    let value = '';
+    value = [...my_string]
+    value.splice(s, overwrite_string.length, overwrite_string)
+    
+    answer = value.join('')
+    return answer;
+}
