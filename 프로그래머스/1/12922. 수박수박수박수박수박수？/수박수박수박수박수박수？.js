82c6bda3593e2b105c1dcf498b5494198f5f8866
tjdwns335 <151831149+tjdwns335@users.noreply.github.com>
2024-01-10 09:42:17 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    var answer = '';
+    for (let i = 0; i < n; i++) {
+        i % 2 === 0 || i === 0 ?
+            answer += '수' : answer += '박';
+    }
+    return answer;
+}
